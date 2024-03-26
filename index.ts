7eee42932a76a8b9a6afcdfab7ad650e90bfe571
Muhammad Ahmed <ahmedarain0012@gmail.com>
2024-03-26 21:26:52 +0500

    assing the line

--- index.ts
+++ index.ts
@@ -1,1 +1,5 @@
 console.log("Hi, im ahmed arain web developer in governor house karachi inshallah one day im successfull person on my family and one go to makkah with my family" );
+if ("im"){
+    console.log("yes im professtional");
+    
+}
